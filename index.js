d1953a21a156c6a2e9a4f49d2a341f7d4a97c0d0
khushbooverma2197 <khushbooverma2197@gmail.com>
2025-11-14 23:36:59 +0530

    Add function to check if a number is prime

---
+++ index.js
@@ -0,0 +1,15 @@
+// Function to check if a number is prime
+function isPrime(num) {
+   if (num <= 1) {
+       return false;
+   }
+   for (let i = 2; i <= Math.sqrt(num); i++) {
+       if (num % i === 0) {
+           return false;
+       }
+   }
+   return true;
+}
+// Example usage:
+//console.log(isPrime(5));     // true
+//console.log(isPrime(10));    // false
